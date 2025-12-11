2e86c38a613da0ff597e4b88be116ab87c842fe6
JMDcoding <jeanmarcdussaud@gmail.com>
2025-12-11 23:29:55 +0100

    commit final

--- ancien_code/IStorage.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface IStorage {
-    void sauvegarderPartie(String mot, boolean gagne, int vies, long duree, List<String> coups);
-    List<String> chargerHistorique();
-}
